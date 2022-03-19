632dc833fca4a2911488a0c769b6457924675fe7
BattleBot <admin@battlebot.kr>
2022-03-19 18:17:22 +0900

    Add

---
+++ musicbot/utils/play_list.py
@@ -0,0 +1,54 @@
+import re
+import discord
+import lavalink
+
+from musicbot.utils.language import get_lan
+from musicbot.utils.statistics import Statistics
+from musicbot import color_code
+
+url_rx = re.compile(r'https?://(?:www\.)?.+')
+
+async def play_list(player, ctx, musics, playmsg):
+    trackcount = 0
+    playmusic = get_lan(ctx.author.id, "music_none")
+    passmusic = get_lan(ctx.author.id, "music_none")
+    loading_dot_count = 0
+    for i in range(0, len(musics)) :
+        loading_dot = ""
+        loading_dot_count += 1
+        if loading_dot_count == 4:
+            loading_dot_count = 1
+        for a in range(0, loading_dot_count):
+            loading_dot = loading_dot + "."
+        embed=discord.Embed(title=get_lan(ctx.author.id, "music_adding_music").format(loading_dot=loading_dot), description=musics[i], color=color_code)
+        await playmsg.edit_original_message(embed=embed)
+        query = musics[i]
+        if not url_rx.match(query):
+            query = f'ytsearch:{query}'
+        nofind = 0
+        while True:
+            results = await player.node.get_tracks(query)
+            if results['loadType'] == 'PLAYLIST_LOADED' or not results or not results['tracks']:
+                if nofind < 3:
+                    nofind += 1
+                elif nofind == 3:
+                    if passmusic == get_lan(ctx.author.id, "music_none"):
+                        passmusic = musics[i]
+                    else:
+                        passmusic = "%s\n%s" %(passmusic, musics[i])
+            else:
+                break
+        track = results['tracks'][0]
+
+        Statistics.up(track['info']['identifier'])
+
+        if playmusic == get_lan(ctx.author.id, "music_none"):
+            playmusic = musics[i]
+        else:
+            playmusic = "%s\n%s" %(playmusic, musics[i])
+        if trackcount != 1:
+            info = track['info']
+            trackcount = 1
+        track = lavalink.models.AudioTrack(track, ctx.author.id, recommended=True)
+        player.add(requester=ctx.author.id, track=track)
+    return info, playmusic, passmusic
