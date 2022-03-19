30d5d69fd1e802d7c38113d6ed8f764f8fe475d7
BattleBot <admin@battlebot.kr>
2022-03-19 18:17:14 +0900

    Add

---
+++ musicbot/utils/language.py
@@ -0,0 +1,28 @@
+import os
+import json
+import sqlite3
+
+def get_lan(id, text):
+    default_language = "en"
+    userdata_db_path = "userdata.db"
+
+    if os.path.exists(userdata_db_path):
+        conn = sqlite3.connect(userdata_db_path, isolation_level=None)
+        c = conn.cursor()
+        c.execute("SELECT * FROM userdata WHERE id=:Id", {"Id": id})
+        temp = c.fetchone()
+        if temp is None:
+            language = default_language
+        else:
+            language = temp[1]
+            if not os.path.exists(f"musicbot/languages/{language}.json"):
+                language = default_language
+        conn.close()
+        with open(f"musicbot/languages/{language}.json") as f:
+            language_data = json.load(f)
+        return language_data[text]
+
+    else:
+        with open(f"musicbot/languages/{default_language}.json") as f:
+            language_data = json.load(f)
+        return language_data[text]
